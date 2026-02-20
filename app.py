f27e17b4c32cf3a7307d6341a27d8b8d1e128c4c
Melih7342 <melih.gueldogdu@stud.fh-campuswien.ac.at>
2026-02-20 20:55:23 +0100

    began with writing process

--- app.py
+++ app.py
@@ -7,13 +7,19 @@ with open('transactions.csv', mode='w') as transactions_file:
     reader = csv.DictReader(transactions_file)
 
     for row in reader:
-        id_val = row['uid']
+        uid_val = row['uid']
         amount_val = float(row['amount'])
 
         if id_val not in summary:
-            summary[id_val] = {'total_amount': 0.0, 'transaction_count': 0}
+            summary[uid_val] = {'total_amount': 0.0, 'transaction_count': 0}
 
-        summary[id_val]['total_amount'] += amount_val
-        summary[id_val]['transaction_count'] += 1
+        summary[uid_val]['total_amount'] += amount_val
+        summary[uid_val]['transaction_count'] += 1
 
+    with open('summary.csv', mode='w') as summary:
+        fieldnames = ['uid', 'total_amount', 'transaction_count']
+        writer = csv.DictWriter(summary, fieldnames=fieldnames)
+        writer.writeheader()
 
+        for uid_val in summary.keys():
+            writer.write(summary[uid_val])
